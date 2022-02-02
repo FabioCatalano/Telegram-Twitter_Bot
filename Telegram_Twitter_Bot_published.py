3dc7f5726a2bd04e68ef42aade765370f2d5ad46
FabioCatalano <98896862+FabioCatalano@users.noreply.github.com>
2022-02-02 17:26:44 +0100

    Update Telegram_Twitter_Bot_published.py

--- Telegram_Twitter_Bot_published.py
+++ Telegram_Twitter_Bot_published.py
@@ -13,7 +13,7 @@ import telebot
 import random
 import html
 
-FT_URL_API='https://your_website/wp-json/wp/v2/posts'
+WEBSITE_URL_API='https://your_website/wp-json/wp/v2/posts'
 
 print('\n Twitter bot')
 
@@ -32,7 +32,7 @@ api=tweepy.API(auth)
 bot = telebot.TeleBot(API_TOKEN, parse_mode=None)
 chat_id='@Chat_name' #insert here the id of the Telegram channel where you want to post your messagges
 
-FILE_NAME='last_article_id.txt'
+FILE_NAME='last_article_id.txt' #id of the last tweeted/posted article
 
 #open last saved article
 def retrieve_last_article(file_name):
@@ -54,39 +54,40 @@ def store_last_article(last_article,file_name):
 while True:
     print("script is running...")
 
-    last_article_id=int(retrieve_last_article(FILE_NAME))
-    r=requests.get(FT_URL_API,headers={'Cache-Control': 'no-cache'})
+    last_article_id=int(retrieve_last_article(FILE_NAME))           #id of the last article on Twitter and Telegram
+    r=requests.get(WEBSITE_URL_API,headers={'Cache-Control': 'no-cache'}) 
     rr=r.json()
-    new_article_id=int(rr[0]['id'])
+    new_article_id=int(rr[0]['id'])   #id of the last article currently on your website
     
     
     if new_article_id!=last_article_id:
         
+	#conting the number of times when the link is not correctly published
         countTW=0
         countTel=0
         
-        author=rr[0]['x_author']
-        link=rr[0]['link']
-        title=html.unescape(rr[0]['title']['rendered'])      
+        author=rr[0]['x_author']  #store author's name
+        link=rr[0]['link']        #store article's link
+        title=html.unescape(rr[0]['title']['rendered'])      #store article's title 
 		
                 
         #send tweet
         try:
-            link_preview=title+ '\n\nBy '+ author + '\n'+link
+            link_preview=title+ '\n\nBy '+ author + '\n'+link     #compose message to tweet
             api.update_status(link_preview)
         except:
             try:
                 time.sleep(60)
-                link_preview=title+ '\n\nBy '+ author + '\n'+link
+                link_preview=title+ '\n\nBy '+ author + '\n'+link  #retry if the first time fails
                 api.update_status(link_preview)
             except:
                 countTW=countTW+1
-                print('Tweet not published',countTW, 'times')
+                print('Tweet not published',countTW, 'times')  #print if the tweet is not published
 
 
         #Send message Telegram
         try:
-            text_telegram='<b>'+title+ '</b>'  + '\n\n'+ link + '\nBy ' + '<em>' + author + '</em>'  
+            text_telegram='<b>'+title+ '</b>'  + '\n\n'+ link + '\nBy ' + '<em>' + author + '</em>'  #compose Telegram message, title is in bold, author in italic
             bot.send_message(chat_id, text_telegram, parse_mode='HTML')
         except:
             try:
@@ -98,9 +99,9 @@ while True:
                 print('Telegram message not published',countTel,'times')  
 				
 
-        store_last_article(new_article_id,FILE_NAME)
+        store_last_article(new_article_id,FILE_NAME)  #store last article's id in the "last_article_id.txt" file
 		
 
-    time.sleep(random.randint(170,190))
+    time.sleep(random.randint(170,190))    #sleep around 3 minutes
     
 
